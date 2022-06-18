4e5e41170b7d07f9e500c16d847d01507d710e56
landrzejewski <landrzejewski.poczta@gmail.com>
2022-06-18 10:39:29 +0200

    mvc

--- src/main/java/pl/training/jakarta/HelloServlet.java
+++ src/main/java/pl/training/jakarta/HelloServlet.java
@@ -7,22 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// @WebServlet(urlPatterns = "say-hello")
+@WebServlet(urlPatterns = "say-hello")
 public class HelloServlet extends HttpServlet {
 
     private static final String NAME_PARAMETER = "name";
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String SAY_HELLO_VIEW = "/say-hello.jsp";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         var name = request.getParameter(NAME_PARAMETER);
-        var htmlResponse = prepareResponse(name);
-        var writer = response.getWriter();
-        writer.println(htmlResponse);
-    }
-
-    private String prepareResponse(String name) {
-        return "<html><head><title>Jakarta</title></head><body><p>Hello %s</p></body></html>"
-                .formatted(name == null ? "" : name);
+        request.setAttribute(NAME_ATTRIBUTE, name == null ? "" : name);
+        getServletContext().getRequestDispatcher(SAY_HELLO_VIEW)
+                .forward(request, response);
     }
 
 }
--- src/main/webapp/say-hello.jsp
+++ src/main/webapp/say-hello.jsp
@@ -11,18 +11,8 @@
     <title>Jakarta</title>
 </head>
 <body>
-<%!
-    private static final String NAME_PARAMETER = "name";
-
-    private String formatName(String name) {
-        return name == null ? "" : name;
-    }
-%>
 <p>
-    Hello <%
-        String name = request.getParameter(NAME_PARAMETER);
-        out.println(formatName(name));
-    %>
+    Hello ${requestScope.name}
 </p>
 </body>
 </html>
